8265d8164c05f18143766dbae7a7f03be5974484
jvpgjava <jjdevv05@gmail.com>
2025-01-28 21:45:27 -0300

    Curso continuacao

---
+++ src/IntroducaoPOO/MembrosEstaticos.java
@@ -0,0 +1,22 @@
+package IntroducaoPOO;
+
+public class MembrosEstaticos {
+
+    /*
+  Membros estáticos
+ • Também chamados membros de classe
+ • Em oposição a membros e instância
+ • São membros que fazem sentido independentemente de objetos. Não
+precisam de objeto para serem chamados. São chamados a partir do
+próprio nome da classe.
+
+ Aplicações comuns:
+ • Classes utilitárias
+ • Declaração de constantes
+ Math.sqrt(double)
+ • Uma classe que possui somente membros estáticos, pode ser uma classe
+estática também. Esta classe não poderá ser instanciada.
+
+
+     */
+}
